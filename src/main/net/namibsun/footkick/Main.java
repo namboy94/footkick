f6934ac757b7cddf3de9b8715e8231e31a4c9b8b
Hermann Krumrey <hermann@krumreyh.com>
2016-05-27 00:32:21 +0200

    Got basic web resource getting working.

--- src/main/net/namibsun/footkick/Main.java
+++ src/main/net/namibsun/footkick/Main.java
@@ -1,14 +1,26 @@
 package net.namibsun.footkick;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 /**
  * The Main Java Class
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
-        System.out.println("Hello World!");
+        String url = "http://www.livescore.com/soccer/germany/bundesliga/";
+        Document document = Jsoup.connect(url).get();
 
+        String question = document.select(".team").text();
+        System.out.println("Question: " + question);
+
+        Elements answerers = document.select(".pts");
+        for (Element answerer : answerers) {
+            System.out.println("Answerer: " + answerer.text());
+        }
     }
 
 }
