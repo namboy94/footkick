c232748b3eefc5f2114cf45d4bd742cb30547f68
Hermann Krumrey <hermann@krumreyh.com>
2016-05-28 00:20:27 +0200

    Added LeagueTable and MatchDay classes.

---
+++ src/main/net/namibsun/footkick/structures/LeagueTable.java
@@ -0,0 +1,7 @@
+package net.namibsun.footkick.structures;
+
+/**
+ * A class that stores the league table standings of a football league
+ */
+public class LeagueTable {
+}
---
+++ src/main/net/namibsun/footkick/structures/MatchDay.java
@@ -0,0 +1,7 @@
+package net.namibsun.footkick.structures;
+
+/**
+ * A class that stores all matches in a particular matchday.
+ */
+public class MatchDay {
+}
