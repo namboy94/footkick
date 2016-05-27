6cc36a9f6a58d7cc1e281434bdbfdf0e184bbaaa
Hermann Krumrey <hermann@krumreyh.com>
2016-05-27 16:33:36 +0200

    Got Matchday script working.

--- src/main/net/namibsun/footkick/Main.java
+++ src/main/net/namibsun/footkick/Main.java
@@ -3,6 +3,10 @@ package net.namibsun.footkick;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The Main Java Class
  */
@@ -15,13 +19,16 @@ public class Main {
 
         Elements leagueTeams = jsoupDocument.select(".team");
         Elements leagueStats = jsoupDocument.select(".pts");
-        Elements matchdayTeam = jsoupDocument.select(".ply");
+        Elements matchdayTeams = jsoupDocument.select(".ply");
         Elements matchdayTimes = jsoupDocument.select(".min");
         Elements matchdayScores = jsoupDocument.select(".sco");
 
         //These Indices start at 1 and 8 to skip the title bar
         int teamPositionIndex = 1;
         int statsPositionIndex = 7;
+
+        ArrayList<Team> teams = new ArrayList<Team>();
+        ArrayList<Match> matches = new ArrayList<Match>();
 
         while(teamPositionIndex < leagueTeams.size() && statsPositionIndex < leagueStats.size()) {
             Team team = new Team(leagueStats.get(statsPositionIndex).text(),
@@ -34,10 +41,38 @@ public class Main {
                                  leagueStats.get(statsPositionIndex + 7).text(),
                                  leagueStats.get(statsPositionIndex + 8).text(),
                                  "" + teamPositionIndex);
-            System.out.println(leagueTeams.get(teamPositionIndex).text());
-            System.out.println(team.goalDifference);
+            teams.add(team);
             teamPositionIndex++;
             statsPositionIndex += 8;
+        }
+
+        for (int i = 0; i < matchdayTeams.size(); i += 2) {
+            Match match = new Match(matchdayTeams.get(i).text(),
+                                    matchdayTeams.get(i + 1).text(),
+                                    matchdayTimes.get(i / 2).text(),
+                                    matchdayScores.get(i / 2).text());
+            matches.add(match);
+
+        }
+
+        for (Match match : matches) {
+            System.out.println(match.awayTeam + match.homeTeam + match.score + match.time);
+        }
+
+    }
+
+    private static class Match {
+
+        String homeTeam;
+        String awayTeam;
+        String time;
+        String score;
+
+        Match(String homeTeam, String awayTeam, String time, String score) {
+            this.homeTeam = homeTeam;
+            this.awayTeam = awayTeam;
+            this.time = time;
+            this.score = score;
         }
 
     }
