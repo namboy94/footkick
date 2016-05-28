d37a93440536969583dbca1567676a837b13a134
Hermann Krumrey <hermann@krumreyh.com>
2016-05-29 01:17:00 +0200

    Removed layout fluff

--- footkick-android/src/main/java/net/namibsun/footkick/MainActivity.java
+++ footkick-android/src/main/java/net/namibsun/footkick/MainActivity.java
@@ -40,38 +40,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        TableLayout table = (TableLayout)findViewById(R.id.table);
+        TableLayout table = (TableLayout) findViewById(R.id.table);
         TableRow newRow = new TableRow(table.getContext());
         TextView text = new TextView(table.getContext());
         text.setText("TEst");
         newRow.addView(text);
         table.addView(newRow);
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
