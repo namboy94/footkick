3864492b0111666892e05e8e5a43f96ce9621c43
Hermann Krumrey <hermann@krumreyh.com>
2016-05-29 13:37:55 +0200

    Adjusted available leagues

--- footkick-java/src/main/java/net/namibsun/footkick/java/scraper/LeagueLister.java
+++ footkick-java/src/main/java/net/namibsun/footkick/java/scraper/LeagueLister.java
@@ -1,18 +1,31 @@
 package net.namibsun.footkick.java.scraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
 /**
  * A class that checks which leagues are available for scraping
  */
 public class LeagueLister {
 
+    /**
+     * Gets the available leagues and eturns them as Strings formatted like this:
+     *          'country league'
+     * @return the array of leagues
+     */
     public static String[] getLeagues() {
 
-        return new String[]{
+        return new String[] {
                 "germany bundesliga",
                 "england premier-league",
-                "italy serie-a"
+                "spain primera-division",
+                "italy serie-a",
+                "namibia premier-league"
         };
-
     }
-
 }
