24b2e6a512814b00d0ea2352a8672e41f7b8038e
Hermann Krumrey <hermann@krumreyh.com>
2017-04-18 09:17:22 +0200

    Ignore .iml, update gradle versions

--- signed_apk_enabler.py
+++
@@ -1,9 +0,0 @@
-android_gradle_file = "footkick-android/build.gradle"
-
-with open(android_gradle_file, 'r') as gradle:
-    content = gradle.read()
-
-content = content.replace("//SIGNED-KEY-RELEASE", "")
-
-with open(android_gradle_file, 'w') as gradle:
-    gradle.write(content)
